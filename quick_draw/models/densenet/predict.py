e47df59891d12c4ec90bc44036b0740aef2e1178
Oleg Polosin <apls777@gmail.com>
2018-10-13 02:33:34 +0100

    submission generation

--- quick_draw/models/densenet/predict.py
+++ quick_draw/models/densenet/predict.py
@@ -1,13 +1,16 @@
-import os
 import pickle
 from quick_draw.models.estimator import predict
 from quick_draw.utils import project_dir
 
 
 if __name__ == '__main__':
-    output_path = project_dir(os.path.join('notebooks', 'data', 'predictions.pickle'))
-    labels_map_path = project_dir(os.path.join('notebooks', 'data', 'labels.json'))
-    predictor = predict('densenet', labels_map_path, [project_dir('notebooks/data/file_1.tfrecords')])
+    labels_map_path = project_dir('data/evaluation/labels.json')
+    # file_path = project_dir('data/evaluation/file_1.tfrecords')
+    # output_path = project_dir('data/evaluation/predictions.pickle')
+    file_path = project_dir('data/submission/test_simplified.tfrecords')
+    output_path = project_dir('data/submission/predictions.pickle')
+
+    predictor = predict('densenet', labels_map_path, [file_path])
 
     predictions = []
     for i, prediction in enumerate(predictor):
